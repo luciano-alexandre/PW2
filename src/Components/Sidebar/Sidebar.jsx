469ef9620877f60fde30b9dc2a5f0a5f14b96abe
Luciano Alexandre de Farias Silva <luciano.cnrn@gmail.com>
2025-06-02 18:42:57 +0000

    Initial commit

---
+++ src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Sidebar.css';
+
+function Sidebar() {
+  return (
+    <aside className="sidebar">
+      <ul>
+        <li>Início</li>
+        <li>Sobre</li>
+        <li>Contato</li>
+      </ul>
+    </aside>
+  );
+}
+
+export default Sidebar;
